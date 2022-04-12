3cd8fa9f9aa0ae7d3a1472db9b505f25e359e48a
natalyarumynina <Natalya.Rumynina@firstlinesoftware.com>
2022-04-12 15:33:47 +0300

    added user and registering

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
